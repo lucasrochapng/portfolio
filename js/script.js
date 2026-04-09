88f104620d5cd3e57be0a5fed02de3664654e9cd
lucas <lucasrochadss@gmail.com>
2026-04-09 03:07:34 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -9,11 +9,34 @@ window.addEventListener("scroll", () => {
     }
 });
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 200;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
+
 
 //Eu sou
 const texts = [
-    "Dev. Web",
-    "Dev. Mobile",
+    "Dev. Front-end",
     "Freelancer"
 ];
 
